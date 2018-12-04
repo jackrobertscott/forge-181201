5b8b5a5f3444f8c45d7d9f00e487cc7d4f3d533c
Jack Scott <mast.j.scott@gmail.com>
2018-12-04 11:22:13 +0800

    pretty storybook

--- packages/forge-desktop/src/stories/index.js
+++ packages/forge-desktop/src/stories/index.js
@@ -6,10 +6,14 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
