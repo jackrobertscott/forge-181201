fa359d8bafd42bb523a12b90c5360cb169ec7350
Jack Scott <mast.j.scott@gmail.com>
2018-12-07 12:56:32 +0800

    make modal dope

--- packages/forge-desktop/src/components/layouts/Modal.tsx
+++ packages/forge-desktop/src/components/layouts/Modal.tsx
@@ -1,10 +1,16 @@
-import React, { ReactNode, FunctionComponent } from 'react';
+import React, {
+  ReactNode,
+  FunctionComponent,
+  useState,
+  useEffect,
+} from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 import styled from 'styled-components';
 import layouts from '../../styles/layouts';
 import shapes from '../../styles/shapes';
 import Toggle, { IToggle } from '../statefuls/Toggle';
 import animate from '../../styles/animate';
+import words from '../../styles/words';
 
 const Wrap = styled('div')`
   ${layouts.center}
@@ -15,18 +21,48 @@ const Wrap = styled('div')`
   animation: ${animate.fadeIn} 0.2s linear;
 `;
 
+const Close = styled('div')`
+  ${words.small}
+  ${layouts.rowsCenter}
+  ${shapes.padded}
+  padding-bottom: 0;
+  margin-bottom: -5px;
+  justify-content: flex-end;
+  cursor: pointer;
+`;
+
 interface IModalProps {
   component: ReactNode;
   children: (bag: IToggle) => ReactNode;
 }
 
 const Modal: FunctionComponent<IModalProps> = ({ component, children }) => {
+  let previousActiveItem: Element | null;
+  useEffect(() => {
+    const execute = () => {
+      previousActiveItem = document.activeElement;
+    };
+    window.addEventListener('click', execute);
+    return () => window.removeEventListener('click', execute);
+  }, []);
   const toggleable = ({ toggle, active, close, ...others }: IToggle) => {
+    const closeNoFocus = () => {
+      if (
+        !previousActiveItem ||
+        previousActiveItem === document.body ||
+        previousActiveItem.tagName === 'BUTTON'
+      ) {
+        close();
+      }
+    };
     const popup = (
       <Wrap>
-        <OutsideClickHandler onOutsideClick={close}>
-          {component}
-        </OutsideClickHandler>
+        <div>
+          <Close onClick={close}>x close.</Close>
+          <OutsideClickHandler onOutsideClick={closeNoFocus}>
+            {component}
+          </OutsideClickHandler>
+        </div>
       </Wrap>
     );
     return (
--- packages/forge-desktop/src/components/modals/ChooseBundleModal.tsx
+++ packages/forge-desktop/src/components/modals/ChooseBundleModal.tsx
@@ -4,6 +4,7 @@ import Split from '../layouts/Split';
 import Card from '../cards/Card';
 import Modal from '../layouts/Modal';
 import { IToggle } from '../statefuls/Toggle';
+import Control from '../inputs/Control';
 
 interface IChooseBundleModalProps extends IComponentProps {
   children: (bag: IToggle) => ReactNode;
@@ -15,7 +16,9 @@ const ChooseBundleModal: FunctionComponent<IChooseBundleModalProps> = ({
   const modal = (
     <Split modal={true}>
       <Card>LALALALA</Card>
-      <Card>Hello world!</Card>
+      <Card>
+        <Control label="hello" help="adsf" />
+      </Card>
     </Split>
   );
   return <Modal component={modal}>{children}</Modal>;
