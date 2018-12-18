26a04dc988853d21083fabe1c6cdbd8eaae8f07c
Jack Scott <jack.rob.scott@gmail.com>
2018-12-18 09:18:22 +0800

    refactored assets

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,16 +1,8 @@
 export interface IGlobalConfigDefaults {
   environment: string;
   appName: string;
-  website: string;
-}
-
-export interface IGlobalConfig extends IGlobalConfigDefaults {
-  debug: boolean;
   assetPath: string;
-  urls: {
-    spa: string;
-    api: string;
-  };
+  website: string;
   stripeKey: string;
   segmentId: string;
   sentryDSN: string;
@@ -22,8 +14,21 @@ const environment = process.env.REACT_APP_NODE_ENV || 'development';
 const defaults: IGlobalConfigDefaults = {
   environment,
   appName: 'Forge',
+  assetPath: './assets',
   website: 'https://useforge.co',
+  stripeKey: process.env.REACT_APP_STRIPE_KEY as string,
+  segmentId: process.env.REACT_APP_SEGMENT_ID as string,
+  sentryDSN: process.env.REACT_APP_SENTRY_DSN as string,
+  intercom: process.env.REACT_APP_INTERCOM_ID as string,
 };
+
+export interface IGlobalConfig extends IGlobalConfigDefaults {
+  debug: boolean;
+  urls: {
+    spa: string;
+    api: string;
+  };
+}
 
 let config: IGlobalConfig;
 
@@ -32,30 +37,20 @@ switch (environment) {
     config = {
       ...defaults,
       debug: false,
-      assetPath: './assets',
       urls: {
         spa: 'https://app.useforge.co',
         api: 'https://api.v1.useforge.co',
       },
-      stripeKey: process.env.REACT_APP_STRIPE_KEY as string,
-      segmentId: process.env.REACT_APP_SEGMENT_ID as string,
-      sentryDSN: process.env.REACT_APP_SENTRY_DSN as string,
-      intercom: process.env.REACT_APP_INTERCOM_ID as string,
     };
     break;
   default:
     config = {
       ...defaults,
       debug: true,
-      assetPath: '/assets',
       urls: {
         spa: 'http://localhost:3000',
         api: 'http://localhost:4000',
       },
-      stripeKey: process.env.REACT_APP_STRIPE_KEY as string,
-      segmentId: process.env.REACT_APP_SEGMENT_ID as string,
-      sentryDSN: process.env.REACT_APP_SENTRY_DSN as string,
-      intercom: process.env.REACT_APP_INTERCOM_ID as string,
     };
     break;
 }
